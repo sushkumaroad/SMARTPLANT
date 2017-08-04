34e087f328f9a020c5056b11eb62d06ee767108d
sushkumaroad <skoad@deakin.edu.au>
2017-08-04 11:04:46 +1000

    Add files via upload

---
+++ t4.2_arduino.ino
@@ -0,0 +1,27 @@
+  /*
+  # Editor     : Lauren
+  # Date       : 13.01.2012
+  # Version    : 1.0
+  # Connect the sensor to the A0(Analog 0) pin on the Arduino board
+  # the sensor value description
+  # 0  ~300     dry soil
+  # 300~700     humid soil
+  # 700~950     in water
+  */
+void setup() {
+  Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop() {
+  int val;
+  val = analogRead(0); //connect sensor to Analog 0
+  Serial.println(val); //print the value to serial port
+  if (val<=300){
+     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  }
+  else {digitalWrite(LED_BUILTIN, LOW);}
+  
+  delay(1000);
+}
+
